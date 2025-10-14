20e36c35bd25f9a241ac752ea4360b62045efe9a
Yutong Chen <chen6yt@mail.uc.edu>
2025-10-13 22:37:24 -0400

    Repo organization & About Page added

---
+++ wimf/app/routes/about.tsx
@@ -0,0 +1,13 @@
+import type { Route } from "./+types/home";
+import { About } from "../pages/about/about";
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "About" },
+    { name: "about", content: "Learn more about us." },
+  ];
+}
+
+export default function About() {
+  return <About />;
+}
