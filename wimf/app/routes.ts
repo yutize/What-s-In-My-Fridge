668e95160e99ab04a942e8dfa4d2593c61c76e60
Yutong Chen <chen6yt@mail.uc.edu>
2025-10-19 17:57:11 -0400

    dashboard page added

--- wimf/app/routes.ts
+++ wimf/app/routes.ts
@@ -3,6 +3,6 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 export default [
     index("routes/home.tsx"),
     route("about", "routes/about.tsx"),
-    route("signup", "routes/signUp.tsx")
-
+    route("signup", "routes/signUp.tsx"),
+    route("dashboard", "routes/dashboard.tsx"),
 ] satisfies RouteConfig;
