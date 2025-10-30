1cf600f210cab72f0da2b6337e7d1299cbe80541
Yutong Chen <chen6yt@mail.uc.edu>
2025-10-30 19:42:39 -0400

    start save

--- wimf/app/middleware/RecipeService/handleRecipeSearch.tsx
+++ wimf/app/middleware/RecipeService/handleRecipeSearch.tsx
@@ -1,9 +1,19 @@
 import type { ActionFunctionArgs } from "react-router";
 import { db } from "../../db/app";
+import axios from "axios";
+import { Link } from "react-router"
 
 export async function handleRecipeSearch({ request }: ActionFunctionArgs) {
   const formData = await request.formData();
+  const entries = Array.from(formData.entries());
+  console.log(process.env.APP_ID, process.env.API_KEY);
+  console.log("Form Data Entries:", entries);
 
-
-  console.log("Form Data Entries:", formData);
+  async function fetchRecipies() {
+    try {
+      const res = await axios.get(
+        "https://api.edamam.com/api/recipes/v2?type=public"
+      )
+    }
+  }
   }
