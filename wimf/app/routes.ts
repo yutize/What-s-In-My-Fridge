a634f603a39724a823cfa99c14b8721d7d5f193f
yutize <chen6yt@mail.uc.edu>
2026-02-02 14:21:34 -0500

    readded route

--- wimf/app/routes.ts
+++ wimf/app/routes.ts
@@ -5,6 +5,7 @@ export default [
     route("about", "routes/about.tsx"),
     route("signup", "routes/signUp.tsx"),
     route("dashboard", "routes/dashboard.tsx"),
+    route("ingredients", "routes/ingredients.tsx"),
     route("logout", "routes/logout.tsx"),
     route("recipes", "routes/recipes.tsx"),
     route("nutrition", "routes/nutrition.tsx"),
