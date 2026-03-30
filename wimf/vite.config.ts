771504f7b46ae9bba48c98e30be8415f373c5663
Yutong Chen <chen6yt@mail.uc.edu>
2026-03-29 20:55:15 -0400

    deployment changes

--- wimf/vite.config.ts
+++ wimf/vite.config.ts
@@ -4,5 +4,6 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  base: '/What-s-In-My-Fridge/',
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });
